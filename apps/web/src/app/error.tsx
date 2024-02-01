3fd36200964a4ee916367d6208481ef01389f4f3
allenabraham777 <allenabraham777@gmail.com>
2024-02-01 23:11:05 +0530

    feat(app-web): add error logger

--- apps/web/src/app/error.tsx
+++ apps/web/src/app/error.tsx
@@ -1,8 +1,17 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 
-const UserError = () => {
+type Props = {
+    error: Error & { digest?: string };
+    reset: () => void;
+};
+
+const UserError = ({ error }: Props) => {
+    useEffect(() => {
+        // Log the error to an error reporting service
+        console.error(error);
+    }, [error]);
     return (
         <div className="h-full w-full flex flex-col gap-10 items-center justify-center">
             <h2 className="text-xl text-foreground/60">Something went wrong!</h2>
